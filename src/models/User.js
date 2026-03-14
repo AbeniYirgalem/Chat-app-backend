a9759ae041843306e3aba84731d2a2a5175119c8
Abeni <abenezeryirgalem0@gmail.com>
2026-03-15 00:09:33 +0300

    feat: create User model with schema for user data

---
+++ src/models/User.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: String,
+    verificationTokenExpires: Date,
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }, // createdAt & updatedAt
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
